5407600ecc4143c328dc3e80510f0a51700b849a
Cássio <cassioalmeidaccti@gmail.com>
2025-01-05 11:03:27 -0300

    feat: add useStante and onChange usage example

---
+++ App.jsx
@@ -0,0 +1,46 @@
+import {useState} from 'react'
+import './App.css'
+
+function App() {
+
+    const [name, setName] = useState("João");
+    const [buttonContent, setButtonContent] = useState("Mudar para José");
+    function changeName() {
+        setName("José");
+        setButtonContent("Nome alterado");
+    }
+
+    let nameWithoutHook = "João";
+    function tryChangeName() {
+        nameWithoutHook = "José";
+    }
+
+    const [repeatedUserInput, setRepeatedUserInput] = useState("");
+
+    return (
+        <>
+            <div>
+                <p>Nome a ser re-renderizado pelo navegador após o React atualizar o seu estado, recriar o VDOM, e
+                    aplicar o que mudou nele ao DOM:</p>
+                <button onClick={changeName}>{buttonContent}:</button>
+                {name}
+                <p>Nome atualizado sem ser pelo React:</p>
+                <button onClick={tryChangeName}>Tentar mudar para José:</button>
+                {nameWithoutHook}
+            </div>
+            <div>
+                <input
+                    placeholder="Insira aqui um nome:"
+                    onChange={
+                        (event) => {
+                            setRepeatedUserInput(event.target.value);
+                        }
+                    }
+                />
+                {repeatedUserInput}
+            </div>
+        </>
+    );
+}
+
+export default App
