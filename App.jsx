ae164e689298bcbeb0756062cb85add1bbba8c19
Cássio <cassioalmeidaccti@gmail.com>
2025-01-05 18:08:13 -0300

    feat: add simple Conditional Rendering usage example

--- App.jsx
+++ App.jsx
@@ -4,10 +4,10 @@ import './App.css'
 function App() {
 
     const [name, setName] = useState("João");
-    const [buttonContent, setButtonContent] = useState("Mudar para José");
+    const [nameButtonContent, setNameButtonContent] = useState("Mudar para José");
     function changeName() {
         setName("José");
-        setButtonContent("Nome alterado");
+        setNameButtonContent("Nome alterado");
     }
 
     let nameWithoutHook = "João";
@@ -17,12 +17,14 @@ function App() {
 
     const [repeatedUserInput, setRepeatedUserInput] = useState("");
 
+    const [showNames, setShowNames] = useState(false);
+
     return (
         <>
             <div>
                 <p>Nome a ser re-renderizado pelo navegador após o React atualizar o seu estado, recriar o VDOM, e
                     aplicar o que mudou nele ao DOM:</p>
-                <button onClick={changeName}>{buttonContent}:</button>
+                <button onClick={changeName}>{nameButtonContent}:</button>
                 {name}
                 <p>Nome atualizado sem ser pelo React:</p>
                 <button onClick={tryChangeName}>Tentar mudar para José:</button>
@@ -38,6 +40,25 @@ function App() {
                     }
                 />
                 {repeatedUserInput}
+            </div>
+            <div>
+                <p>Também mostrar, abaixo, os nomes?</p>
+                <button
+                    onClick={
+                        () => {
+                            setShowNames(!showNames)
+                        }
+                    }
+                >
+                    Mostrar, ou não, os nomes:
+                </button>
+                {showNames && (
+                    <ul>
+                        <li>{name}</li>
+                        <li>{nameWithoutHook}</li>
+                        <li>{repeatedUserInput}</li>
+                    </ul>
+                )}
             </div>
         </>
     );
