05d7d1203ee0e729e006cd7024296589902ee59e
agithubuser-9 <kassioflex@gmail.com>
2025-01-06 17:54:30 -0300

    fix: Change updated css and jsx files location
    Mistake made while comitting using the github browser version. Using the cli version from now on.

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import './App.css'
 function App() {
 
     const [name, setName] = useState("João");
-    const [buttonContent, setButtonContent] = useState("Mudar para José");
+    const [nameButtonContent, setNameButtonContent] = useState("Mudar para José");
     function changeName() {
         setName("José");
-        setButtonContent("Nome alterado");
+        setNameButtonContent("Nome alterado");
     }
 
     let nameWithoutHook = "João";
@@ -15,15 +15,52 @@ function App() {
         nameWithoutHook = "José";
     }
 
+    const [repeatedUserInput, setRepeatedUserInput] = useState("");
+
+    const [showNames, setShowNames] = useState(false);
+
     return (
-        <div>
-            <p>Nome a ser re-renderizado pelo navegador após o React atualizar o seu estado, recriar o VDOM, e  aplicar o que mudou nele ao DOM:</p>
-            <button onClick={changeName}>{buttonContent}:</button>
-            {name}
-            <p>Nome atualizado sem ser pelo React:</p>
-            <button onClick={tryChangeName}>Tentar mudar para José:</button>
-            {nameWithoutHook}
-        </div>
+        <>
+            <div>
+                <p>Nome a ser re-renderizado pelo navegador após o React atualizar o estado da variável, criar outra versão do VDOM, comparar com a versão anterior, e
+                    aplicar as mudanças ao DOM:</p>
+                <button onClick={changeName}>{nameButtonContent}:</button>
+                {name}
+                <p>Nome atualizado sem ser pelo React:</p>
+                <button onClick={tryChangeName}>Tentar mudar para José:</button>
+                {nameWithoutHook}
+            </div>
+            <div>
+                <input
+                    placeholder="Insira aqui um nome:"
+                    onChange={
+                        (event) => {
+                            setRepeatedUserInput(event.target.value);
+                        }
+                    }
+                />
+                {repeatedUserInput}
+            </div>
+            <div>
+                <p>Também mostrar, abaixo, os nomes?</p>
+                <button
+                    onClick={
+                        () => {
+                            setShowNames(!showNames);
+                        }
+                    }
+                >
+                    Mostrar, ou não, os nomes:
+                </button>
+                {showNames && (
+                    <ul>
+                        <li>{name}</li>
+                        <li>{nameWithoutHook}</li>
+                        <li>{repeatedUserInput}</li>
+                    </ul>
+                )}
+            </div>
+        </>
     );
 }
 
